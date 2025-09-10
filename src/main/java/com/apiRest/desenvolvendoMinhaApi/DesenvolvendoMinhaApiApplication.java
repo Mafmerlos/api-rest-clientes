f75f488cda55f9496beef8a2cf50b058f54ec7c4
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-09 22:02:29 -0300

    feat: Commit inicial da API de Gerenciamento de Clientes
    Projeto inicial da API REST para gerenciamento de clientes, desenvolvido com Spring Boot. A API expõe endpoints para listar e criar clientes, utilizando um banco de dados em memória H2 para persistência.

---
+++ src/main/java/com/apiRest/desenvolvendoMinhaApi/DesenvolvendoMinhaApiApplication.java
@@ -0,0 +1,15 @@
+package com.apiRest.desenvolvendoMinhaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvolvendoMinhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvolvendoMinhaApiApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/apiRest/desenvolvendoMinhaApi/DesenvolvendoMinhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiRest.desenvolvendoMinhaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvendoMinhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
